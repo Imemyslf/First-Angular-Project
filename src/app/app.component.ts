9b315ed1a994997dda0929b1022c481cc5bd7fe1
Imemyslf <kishanbro9666@gmail.com>
2025-08-21 17:57:53 +0530

    feat: partial work on user component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 // import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-
+import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
 //RouterOutlet,
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  imports: [HeaderComponent, UserComponent],
 })
 export class AppComponent {
-  title = 'first-angular-app';
+  users = DUMMY_USERS;
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, Input, input, computed } from '@angular/core';
+// import { Component, signal, computed } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
 
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 @Component({
   selector: 'app-user',
-  imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
 
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+
+  // imagePath = computed(() => `assets/images/users/${this.avatar()}`);
+  get imagePath() {
+    return `assets/images/users/${this.avatar}`;
+  }
+
+  onSelectUser() {}
 }
+
+// export class UserComponent {
+//   // selectedUser = DUMMY_USERS[randomIndex];
+//   selectedUser = signal(DUMMY_USERS[randomIndex]);
+//   imagePath = computed(
+//     () => `assets/images/users/${this.selectedUser().avatar}`
+//   );
+//   // get imagePath() {
+//   //   return `assets/images/users/${this.selectedUser.avatar}`;
+//   // }
+
+//   onSelectUser() {
+//     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+//     // this.selectedUser = DUMMY_USERS[randomIndex];
+//     this.selectedUser.set(DUMMY_USERS[randomIndex]);
+//     console.log('Selected User:', this.selectedUser);
+//   }
+// }
